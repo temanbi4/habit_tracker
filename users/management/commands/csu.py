c72fd59edd3c11c4a20a50fba6e2198d0ad69832
temanbi4 <temanbi4@gmail.com>
2023-11-19 22:13:43 +0300

    created csu add urls for users

---
+++ users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    """Кастомная команда для создания суперпользователя"""
+
+    def handle(self, *args, **kwargs):
+        user = User.objects.create(email='admin@admin.admin',
+                                   first_name='Artem',
+                                   last_name='ADMIN',
+                                   is_staff=True,
+                                   is_superuser=True)
+
+        user.set_password('admin')
+        user.save()
