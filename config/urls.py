6defca54c6512849dbab26bfa337d941d6512ccb
temanbi4 <temanbi4@gmail.com>
2023-11-20 16:26:37 +0300

    changed some files with flake8

--- config/urls.py
+++ config/urls.py
@@ -36,6 +36,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('spa.urls', namespace='spa')),
     path('users/', include('users.urls', namespace='users')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
